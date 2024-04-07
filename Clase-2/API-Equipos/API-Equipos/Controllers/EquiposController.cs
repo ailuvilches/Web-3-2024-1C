1f6de82b71beb5b4c4bd51d63002ad451cc3c189
Ailu Vilches <ailenpereiravilches@gmail.com>
2024-04-07 17:34:06 -0300

    Web API to list football teams without persistence

---
+++ Clase-2/API-Equipos/API-Equipos/Controllers/EquiposController.cs
@@ -0,0 +1,37 @@
+﻿namespace API_Equipos.Controllers;
+
+using API_Equipos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EquiposController : ControllerBase
+{
+    private static List<Equipo> equipos = new List<Equipo>();
+
+    // GET: api/Equipos
+    [HttpGet]
+    public IEnumerable<Equipo> Get()
+    {
+        return equipos;
+    }
+
+    // POST: api/Equipos
+    [HttpPost]
+    public void Post([FromBody] Equipo equipo)
+    {
+        equipos.Add(equipo);
+    }
+
+    // DELETE: api/Equipos/nombreDelEquipo
+    [HttpDelete("{nombre}")]
+    public void Delete(string nombre)
+    {
+        var equipo = equipos.Find(e => e.Nombre == nombre);
+        if (equipo != null)
+        {
+            equipos.Remove(equipo);
+        }
+    }
+}
---
+++ Clase-2/API-Equipos/API-Equipos/Models/Equipo.cs
@@ -0,0 +1,7 @@
+﻿namespace API_Equipos.Models;
+
+public class Equipo
+{
+    public string Nombre { get; set; }
+    public string Pais { get; set; }
+}
